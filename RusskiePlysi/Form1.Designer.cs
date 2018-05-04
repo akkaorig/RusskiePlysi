067d33d2a75bce327eadfe6ee393df8a933a1169
Vlad <35502476+TheDevFromKer@users.noreply.github.com>
2018-05-04 20:15:33 +0300

    Add files via upload

---
+++ RusskiePlysi/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace RusskiePlysi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Перевести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(651, 438);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "#подключить <студия>\n#подключить <системная>\n\nфсз тело() {\n     вывести(\"привет м" +
+    "ир!\");\n     подождать;\n     выйти;\n}";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 495);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Русские Плюсы++";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ RusskiePlysi/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RusskiePlysi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string temp = richTextBox1.Text;
+            temp = temp.Replace("#подключить","#include");
+            temp = temp.Replace("<студия>", "<stdio.h>");
+            temp = temp.Replace("<системная>", "<windows.h>");
+            temp = temp.Replace("фсз ", "int ");
+            temp = temp.Replace("тело(", "main(");
+            temp = temp.Replace("вывести(", "printf(");
+            temp = temp.Replace("подождать;", "system(\"pause\");");
+            temp = temp.Replace("выйти;}", "return 0;");
+            temp = temp.Replace("вернуть ", "return ");
+            temp = temp.Replace("число ", "int ");
+            temp = temp.Replace("строка ", "std::string ");
+            temp = temp.Replace("символ ", "char ");
+            temp = temp.Replace("символы ", "char *");
+            temp = temp.Replace("нвз ", "void ");
+            temp = temp.Replace("завершить;", "ExitProcess(0);");
+            richTextBox1.Text = temp;
+        }
+    }
+}
